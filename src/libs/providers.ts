d1365cc110b3f029c81c54b96b5e7a6f03781f5e
Noe <noemk2@gmail.com>
2023-05-17 00:20:55 +0000

    mainnet finish

--- src/libs/providers.ts
+++ src/libs/providers.ts
@@ -4,5 +4,7 @@ import { CurrentConfig } from '../config'
 // Provider Functions
 
 export function getProvider(): providers.Provider {
-  return new ethers.providers.JsonRpcProvider(CurrentConfig.rpc.mainnet)
+  // return new ethers.providers.JsonRpcProvider(CurrentConfig.rpc.mainnet)
+  return new ethers.providers.Web3Provider(window.ethereum);
+
 }
