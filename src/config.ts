ce7844991d04c4680d43350a254c3550cd353e61
Noe <noemk2@gmail.com>
2023-05-17 00:03:02 +0000

    finis

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,8 @@
-import { SupportedChainId, Token } from '@uniswap/sdk-core'
+import { Token } from '@uniswap/sdk-core'
 import { FeeAmount } from '@uniswap/v3-sdk'
 import { USDC_TOKEN, WETH_TOKEN } from './libs/constants'
 
 // Inputs that configure this example to run
-const NEO = new Token(SupportedChainId.CELO_ALFAJORES, '0xb9095b079195690907A40399F7F9A7bb9F9F460C', 18, 'NEO', 'Neo tokens')
-
-const CELO = new Token(SupportedChainId.CELO_ALFAJORES, '0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9', 18, "CELO", "Celo Native asset")
-
-
 export interface ExampleConfig {
   rpc: {
     local: string
@@ -24,15 +19,14 @@ export interface ExampleConfig {
 // Example Configuration
 
 export const CurrentConfig: ExampleConfig = {
-
   rpc: {
     local: 'http://localhost:8545',
-    mainnet: 'https://alfajores-forno.celo-testnet.org',
+    mainnet: 'https://mainnet.infura.io/v3/',
   },
   tokens: {
-    in: NEO,
-    amountIn: 1,
-    out: CELO,
+    in: USDC_TOKEN,
+    amountIn: 1000,
+    out: WETH_TOKEN,
     poolFee: FeeAmount.MEDIUM,
   },
 }
--- src/libs/quote.ts
+++ src/libs/quote.ts
@@ -1,12 +1,8 @@
 import { ethers } from 'ethers'
 import { CurrentConfig } from '../config'
 import { computePoolAddress } from '@uniswap/v3-sdk'
-// import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
-import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json'
-
+import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
-
-
 import {
   POOL_FACTORY_CONTRACT_ADDRESS,
   QUOTER_CONTRACT_ADDRESS,
